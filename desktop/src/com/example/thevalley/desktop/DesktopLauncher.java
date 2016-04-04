08c00b171d0bd249a2048bc35788d3cf40d6b034
Bram <bram209@hotmail.com>
2016-04-04 11:22:14 +0200

    initial commit

---
+++ desktop/src/com/example/thevalley/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.example.thevalley.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.example.thevalley.TheValleyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TheValleyGame(), config);
+	}
+}
