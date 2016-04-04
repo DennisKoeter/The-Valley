8a9159c9ceddb084782d61a2ecb906ae6a48d8db
Edwin <edwinvrooij@gmail.com>
2016-04-04 16:26:25 +0200

    New project initiated.

---
+++ desktop/src/com/groeps33/valley/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.groeps33.valley.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.groeps33.valley.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
