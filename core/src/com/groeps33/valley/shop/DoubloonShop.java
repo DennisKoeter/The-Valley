72863b2bbcc7dd0b21fd8543d618d4ffb0d65247
denniskoeter <dennis.koeter@hotmail.com>
2016-04-06 14:24:17 +0200

    implemented DoubloonShop

--- core/src/com/groeps33/valley/shop/DoubloonShop.java
+++ core/src/com/groeps33/valley/shop/DoubloonShop.java
@@ -1,7 +1,42 @@
 package com.groeps33.valley.shop;
 
+import com.groeps33.valley.entity.Character;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by Dennis on 06/04/16.
  */
 public class DoubloonShop {
+    List<Statboost> statboosts;
+
+    public DoubloonShop(){
+        this.statboosts = new ArrayList<Statboost>();
+    }
+
+    public void addStatboost(Statboost statboost){
+        this.statboosts.add(statboost);
+    }
+
+    public Iterator<Statboost> getStatboosts(){
+        return this.statboosts.iterator();
+    }
+
+    public Statboost findStatboost(String statboostName){
+        for(Statboost statboost : this.statboosts){
+            if (statboost.getName().equals(statboostName)){
+                return statboost;
+            }
+        }
+        return null;
+    }
+
+    public Statboost buyStatboost(String statboostName, Character customer){
+        Statboost statboost = this.findStatboost(statboostName);
+        if(statboost == null) return null;
+        customer.boostStat(statboost);
+        return statboost;
+    }
 }
