a641bf184a4f3611f9b32e5ce9506047593fa790
Edwin <edwinvrooij@gmail.com>
2016-04-06 16:16:58 +0200

    added test classes

---
+++ core/src/test/java/com/groeps33/valley/chat/ChatMessageTest.java
@@ -0,0 +1,17 @@
+package com.groeps33.valley.chat;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Edwin
+ *         Created on 4/6/2016
+ */
+public class ChatMessageTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
---
+++ core/src/test/java/com/groeps33/valley/constants/ConstTest.java
@@ -0,0 +1,17 @@
+package com.groeps33.valley.constants;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Edwin
+ *         Created on 4/6/2016
+ */
+public class ConstTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
