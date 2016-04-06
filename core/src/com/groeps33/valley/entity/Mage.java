32a955e461094a1799b8fa7fe161d636fc6414b1
Edwin <edwinvrooij@gmail.com>
2016-04-06 15:10:14 +0200

    updated mage

--- core/src/com/groeps33/valley/entity/Mage.java
+++ core/src/com/groeps33/valley/entity/Mage.java
@@ -6,20 +6,18 @@ package com.groeps33.valley.entity;
  */
 public class Mage extends Character{
 
-    private int healAmount;
+    private int fireBallDmg;
 
-    /**
-     * A type of character with the class Mage
-     * @param x
-     * @param y
-     * @param name
-     * @param maxHp
-     * @param defence
-     * @param attackDamage
-     * @param moveSpeed
-     */
-    public Mage(float x, float y, String name, int maxHp, int defence, int attackDamage, int moveSpeed) {
-        super(x, y, name, maxHp, defence, attackDamage, moveSpeed);
+    public int getFireBallDmg() {
+        return fireBallDmg;
     }
 
+    public void setFireBallDmg(int fireBallDmg) {
+        this.fireBallDmg = fireBallDmg;
+    }
+
+    public Mage(float x, float y, String name, int maxHp, int defence, int attackDamage, int moveSpeed, int fireBallDmg) {
+        super(x, y, name, maxHp, defence, attackDamage, moveSpeed);
+        this.fireBallDmg = fireBallDmg;
+    }
 }
