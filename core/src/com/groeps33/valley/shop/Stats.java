4eaa71794545b84282e614c9f7173c58da3b67fd
summerke <robin.welten@planet.nl>
2016-04-06 14:34:01 +0200

    stats enum

---
+++ core/src/com/groeps33/valley/shop/Stats.java
@@ -0,0 +1,18 @@
+package com.groeps33.valley.shop;
+
+/**
+ * Created by Robin on 6-4-2016.
+ */
+
+enum Stat
+    {
+        ATTACK_DAMAGE,
+        MOVE_SPEED,
+        DEFENC,
+        MAX_HP,
+        CURRENT_HP
+
+    }
+
+
+
