a132c026f00b6140afc5f9715c4ec48d39d2aaa6
Edwin <edwinvrooij@gmail.com>
2016-04-10 20:41:52 +0200

    added gamescreen test, isn t going to do anything tho

---
+++ core/src/test/java/com/groeps33/valley/screens/GameScreenTest.java
@@ -0,0 +1,58 @@
+package com.groeps33.valley.screens;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Edwin
+ *         Created on 4/10/2016
+ */
+public class GameScreenTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void show() throws Exception {
+
+    }
+
+    @Test
+    public void render() throws Exception {
+
+    }
+
+    @Test
+    public void checkCollisionWithMap() throws Exception {
+
+    }
+
+    @Test
+    public void resize() throws Exception {
+
+    }
+
+    @Test
+    public void pause() throws Exception {
+
+    }
+
+    @Test
+    public void resume() throws Exception {
+
+    }
+
+    @Test
+    public void hide() throws Exception {
+
+    }
+
+    @Test
+    public void dispose() throws Exception {
+
+    }
+
+}
