f82ca04e808e4fe7bbd39dc05606812a0bb261f3
Edwin <edwinvrooij@gmail.com>
2016-04-10 20:54:11 +0200

    added highscore test class

---
+++ core/src/test/java/com/groeps33/valley/highscore/HighScoreTest.java
@@ -0,0 +1,33 @@
+package com.groeps33.valley.highscore;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Edwin
+ *         Created on 4/10/2016
+ */
+public class HighScoreTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void getInstance() throws Exception {
+
+    }
+
+    @Test
+    public void clearBoard() throws Exception {
+
+    }
+
+    @Test
+    public void generateBoard() throws Exception {
+
+    }
+
+}
