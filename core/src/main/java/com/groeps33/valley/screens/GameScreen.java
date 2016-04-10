4f84055c88613d97ab0f17c9b95daa7e45b5480e
Bram <b.hoendervangers@student.fontys.nl>
2016-04-10 14:20:02 +0200

    achtergrond verandert van rood > zwart

--- core/src/main/java/com/groeps33/valley/screens/GameScreen.java
+++ core/src/main/java/com/groeps33/valley/screens/GameScreen.java
@@ -76,7 +76,7 @@ public class GameScreen extends TheValleyScreen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         camera.position.set(character.getLocation().x, character.getLocation().y, 0);
