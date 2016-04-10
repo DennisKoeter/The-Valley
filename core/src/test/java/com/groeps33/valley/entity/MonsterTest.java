afd0ec65f205f915043efb50097b659fd5131a17
Edwin <edwinvrooij@gmail.com>
2016-04-10 21:04:15 +0200

    monster test added

---
+++ core/src/test/java/com/groeps33/valley/entity/MonsterTest.java
@@ -0,0 +1,40 @@
+package com.groeps33.valley.entity;
+
+import com.badlogic.gdx.math.Rectangle;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Edwin
+ *         Created on 4/10/2016
+ */
+public class MonsterTest {
+
+    private int locX = 10;
+    private int locY = 15;
+    private String name = "Blazeit";
+    private int maxHp = 100;
+    private int defence = 25;
+    private int ad = 10;
+    private int ms = 5;
+    private Monster monster;
+
+    private int width = 5;
+    private int height = 5;
+
+    @Before
+    public void setUp() throws Exception {
+        monster = new Monster(locX, locY, name, maxHp, defence, ad, ms);
+    }
+
+    @Test
+    public void getBounds() throws Exception {
+        Rectangle actual =  monster.getBounds();
+        Rectangle expected = new Rectangle(locX, locY, width, height);
+
+        assertEquals(actual, expected);
+    }
+
+}
