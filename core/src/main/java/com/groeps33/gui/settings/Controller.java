24eafc11454eae2b3d80299577809bf13ef8396d
denniskoeter <dennis.koeter@hotmail.com>
2016-05-25 11:16:58 +0200

    added stuff

---
+++ core/src/main/java/com/groeps33/gui/settings/Controller.java
@@ -0,0 +1,7 @@
+package com.groeps33.gui.settings;
+
+/**
+ * Created by Dennis on 25/05/16.
+ */
+public class Controller {
+}
---
+++ core/src/main/java/com/groeps33/gui/shop/Controller.java
@@ -0,0 +1,7 @@
+package com.groeps33.gui.shop;
+
+/**
+ * Created by Dennis on 25/05/16.
+ */
+public class Controller {
+}
