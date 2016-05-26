61f7094b592618a04babc1566e17595ebfa767e5
denniskoeter <dennis.koeter@hotmail.com>
2016-05-26 15:39:10 +0200

    settings are now saved in ValleyFX

--- gui/src/com/groeps33/gui/settings/Controller.java
+++ gui/src/com/groeps33/gui/settings/Controller.java
@@ -2,9 +2,8 @@ package com.groeps33.gui.settings;
 
 import com.groeps33.gui.ValleyFX;
 import javafx.fxml.FXML;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import javafx.scene.control.CheckBox;
 
-import java.awt.*;
 import java.io.IOException;
 
 /**
@@ -12,14 +11,16 @@ import java.io.IOException;
  */
 public class Controller {
     @FXML
-    Checkbox audioFxCheckbox;
+    CheckBox audioFxCheckbox;
 
     @FXML
-    Checkbox audioMusicCheckbox;
+    CheckBox audioMusicCheckbox;
 
     @FXML
     private void back() throws IOException {
-        //TODO implement checkbox selection
+        boolean audioFX = audioFxCheckbox.isSelected();
+        boolean audioMusic = audioMusicCheckbox.isSelected();
+        ValleyFX.saveSettings(audioFX, audioMusic);
         ValleyFX.changeScene(getClass().getResource("../menu/menu.fxml"));
     }
 }
