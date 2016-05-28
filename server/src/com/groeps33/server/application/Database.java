cbca9707396e4a35ef22b79709fd0e2a51dbc9f1
Edwin <edwinvrooij@gmail.com>
2016-05-28 19:07:42 +0200

    fixed login

--- server/src/com/groeps33/server/application/Database.java
+++ server/src/com/groeps33/server/application/Database.java
@@ -1,9 +1,10 @@
 package com.groeps33.server.application;
 
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import com.groeps33.server.shared.UserAccount;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -15,6 +16,13 @@ import java.sql.Statement;
 public class Database {
 
     private final MysqlDataSource dataSource;
+    private static final String ACCOUNT_ID = "id";
+    private static final String ACCOUNT_USERNAME = "username";
+    public static final String ACCOUNT_PASSWORD = "password";
+    public static final String ACCOUNT_EMAIL = "email";
+    public static final String ACCOUNT_DOUBLOONS = "doubloons";
+    public static final String ACCOUNT_NAME_CHANGE = "name_change";
+    public static final String ACCOUNT_NAME_COLOR = "name_color";
 
     public Database() {
         dataSource = new MysqlDataSource();
@@ -26,7 +34,7 @@ public class Database {
     public UserAccount register(String username, String email, String password) {
         try (Connection c = dataSource.getConnection()) {
             try (Statement s = c.createStatement()) {
-                int rowsAffected = s.executeUpdate(String.format("INSERT INTO `the-valley-db`.`account` (`username`, `password`, `email`) VALUES ('%s', '%s', '%s')", username, email, password));
+                int rowsAffected = s.executeUpdate(String.format("INSERT INTO `the-valley-db`.`account` (`%s`, `%s`, `%s`) VALUES ('%s', '%s', '%s')", ACCOUNT_USERNAME, ACCOUNT_PASSWORD, ACCOUNT_EMAIL,  username, email, password));
                 if (rowsAffected == 1) {
                     return new UserAccount(username, email);
                 } else {
@@ -37,5 +45,23 @@ public class Database {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public UserAccount login(String username, String password) throws Exception {
+        try (Connection c = dataSource.getConnection()) {
+            try (Statement s = c.createStatement()) {
+                String query = String.format("SELECT * FROM `the-valley-db`.account WHERE %s = '%s' AND %s = '%s'", ACCOUNT_USERNAME, username, ACCOUNT_PASSWORD, password);
+                try (ResultSet rs = s.executeQuery(query)) {
+                    if (!rs.next()) {
+                        System.out.format("No account was found for username %s and password %s", username, password);
+                        return null;
+                    } else {
+                        String email = rs.getString(ACCOUNT_EMAIL);
+                        System.out.format("Successfully returned account with username %s and email %s", username, email);
+                        return new UserAccount(username, email);
+                    }
+                }
+            }
+        }
     }
 }
