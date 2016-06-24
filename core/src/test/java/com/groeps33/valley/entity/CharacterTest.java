1fba2d13208f07d230f6baa1c93d4180bbb71d5e
Edwin <edwinvrooij@gmail.com>
2016-06-24 13:21:06 +0200

    fixed resolution of some images

--- core/src/test/java/com/groeps33/valley/entity/CharacterTest.java
+++ core/src/test/java/com/groeps33/valley/entity/CharacterTest.java
@@ -1,5 +1,6 @@
 package com.groeps33.valley.entity;
 
+import com.badlogic.gdx.math.Vector2;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,9 +18,18 @@ public class CharacterTest {
     private final String NAME = "Naam";
     private PlayerClass PLAYERCLASS = PlayerClass.ARCHER;
 
+    private Projectile projectile;
+
+    private final double ANGLE = 10;
+    private final Vector2 LOCATION = new Vector2(X, Y);
+    private final Vector2 VELOCITY = new Vector2(X, Y);
+    private static final int DAMAGE = 25;
+    private static final int ATTACKRANGE = 400;
+
     @Before
     public void setUp() throws Exception {
         character = new Character(X, Y, NAME, PLAYERCLASS);
+        projectile = new Projectile(ANGLE, LOCATION, VELOCITY, DAMAGE, ATTACKRANGE);
     }
 
     @After
@@ -83,7 +93,8 @@ public class CharacterTest {
 
     @Test
     public void getProjectiles() throws Exception {
-
+        character.addProjectile(projectile);
+        assertEquals("Projectiles did not match", projectile, character.getProjectiles().get(0));
     }
 
     @Test
