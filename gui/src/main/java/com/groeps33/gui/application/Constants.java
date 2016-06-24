6562f7342f99430d415989a1afc5ad02e96e7bc7
denniskoeter <dennis.koeter@hotmail.com>
2016-06-24 14:51:50 +0200

    added monster sprites

--- gui/src/main/java/com/groeps33/gui/application/Constants.java
+++ gui/src/main/java/com/groeps33/gui/application/Constants.java
@@ -9,8 +9,8 @@ public class Constants {
     public static final String AUDIOFX_KEY = "audioFX";
     public static final String AUDIOMUSIC_KEY = "audioMusic";
 
-//    public static final String RMI_IP = "169.254.206.217";
-    public static final String RMI_IP = "127.0.0.1";
+    public static final String RMI_IP = "169.254.206.217";
+//    public static final String RMI_IP = "127.0.0.1";
 
     public static final String SETTINGS_PATH = "../screens/settings/settings.fxml";
     public static final String HIGHSCORES_PATH = "../screens/highscores/highscores.fxml";
